3de8f5e6e97b5be3de540a40a2a1b9081ff04bf7
chandra <sekar_vishaal@yahoo.co.in>
2021-09-28 02:24:15 +0530

    discord

--- src/components/Socialmediabar.js
+++ src/components/Socialmediabar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import 'font-awesome/css/font-awesome.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../assets/css/sbar.css'
+import dcar from '../assets/img/dcard.png'
 
 export default class Socialmediabar extends Component {
     
@@ -10,7 +11,7 @@ export default class Socialmediabar extends Component {
             <div>  
                   
                   <a href="https://twitter.com/SoccerPunksETH?s=20" className="fa fa-twitter p-3 sbar"></a>
-                  <a href="https://discord.gg/Ch7Nz7C4fW" className="fab fa-discord p-3 sbar"></a>
+                  <a href="https://discord.gg/Ch7Nz7C4fW" class="sbar"><img src={dcar} /></a>
             </div>
         )
     }
