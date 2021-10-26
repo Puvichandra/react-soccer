68153d2acfc8c65e102ea684d1809af46567880f
chandra <sekar_vishaal@yahoo.co.in>
2021-10-26 22:09:13 +0530

    last26102021

--- src/index.js
+++ src/index.js
@@ -14,7 +14,12 @@ ReactDOM.render(
   </React.StrictMode>,
  
   document.getElementById('root')
+
 );
+ // this is the code added to redirect ssl
+var sslRedirect = require('heroku-ssl-redirect');
+const app = express();
+app.use(sslRedirect());
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
